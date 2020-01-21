37315abbad4e28dec9056cecc69a15ce800c208e
Larissa e Icaro <larissa.silva86@gmail.com>
2020-01-20 22:15:07 -0300

    WIP: Admin edit validates category fields
    Co-authored-by: Icaro de Siqueira <icarorob@hotmail.com>

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,32 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.create(category_params)
+    if @category.persisted?
+      redirect_to @category
+    else
+      flash.now[:alert] = 'Erro, todos os dados devem ser preenchidos'
+      render :new
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.update(category_params)
+    redirect_to @category
+  end
   private
 
   def category_params
---
+++ spec/features/admin/admin_register_categories_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+feature 'Admin register categories' do
+  scenario 'successfully' do
+    admin = create(:user, role: :admin)
+
+    login_as admin, scope: :user
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Nova Categoria'
+
+    fill_in 'Nome', with: 'A'
+    fill_in 'Diária padrão', with: 100
+    fill_in 'Seguro padrão do carro', with: 500
+    fill_in 'Seguro padrão contra terceiros', with: 300
+    click_on 'Enviar'
+    
+    expect(page).to have_content('Categoria: A')
+    expect(page).to have_content(100)
+    expect(page).to have_content(500)
+    expect(page).to have_content(300)
+  end
+
+  scenario 'failure' do
+    admin = create(:user, role: :admin)
+
+    login_as admin, scope: :user
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Nova Categoria'
+
+    fill_in 'Nome', with: ''
+    fill_in 'Diária padrão', with: 100
+    fill_in 'Seguro padrão do carro', with: 500
+    fill_in 'Seguro padrão contra terceiros', with: 300
+    click_on 'Enviar'
+    
+
+    expect(page).to have_content('Erro, todos os dados devem ser preenchidos')
+    expect(current_path).to eq new_category_path
+  end
+
+  scenario 'admin edit category' do
+    admin = create(:user, role: :admin)
+    category = create(:category, name: 'A', daily_rate: 150, car_insurance: 500, third_party_insurance: 300)
+    login_as admin, scope: :user
+
+    visit root_path
+    click_on 'Categorias'
+    click_on category.name
+    click_on 'Editar Categoria'
+
+    fill_in 'Nome', with: 'B'
+    fill_in 'Diária padrão', with: 150
+    fill_in 'Seguro padrão do carro', with: 200
+    click_on 'Enviar'
+    
+    expect(page).to have_content('Categoria: B')
+    expect(page).to have_content('R$ 150,00')
+    expect(page).to have_content('R$ 200,00')
+    expect(page).to have_content('R$ 300,00')
+  end
+end
